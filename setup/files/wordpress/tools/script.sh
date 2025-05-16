0ef9fdfb97afe4ff57d47344e9ea60dc6d2a563a
Zakariae Ezzine <zezzine@e1r1p2.1337.ma>
2025-05-16 16:48:48 +0100

    new commit

--- setup/files/wordpress/tools/script.sh
+++ setup/files/wordpress/tools/script.sh
@@ -6,7 +6,7 @@ sleep 2
 
 wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="mariadb" --allow-root
 
-wp core install --url="https://aahrach.42.fr" --title="$WP_TITLE" --admin_name=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp core install --url="https://zackahr.duckdns.org" --title="$WP_TITLE" --admin_name=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
 
 wp user create  $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
 
